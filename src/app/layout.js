99e4516577ea54ee959bc75d25173b26c484697a
Artem Furman <artemfurman@gmail.com>
2023-08-14 07:31:22 +0200

    test overflow-hidden on html

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className="bg-gray-100" lang="en">
+    <html className="bg-gray-100 overflow-hidden" lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )

90f6ea0b9094b8a33aee66d077a53557209d94dc
Artem Furman <artemfurman@gmail.com>
2023-08-14 06:59:51 +0200

    paddings fixed

--- src/app/snap/page.js
+++ src/app/snap/page.js
@@ -20,10 +20,10 @@ export default function Home() {
             </section>
             <section
                 id="skills-section"
-                className="snap-start h-[100svh] w-screen pt-14 flex flex-col"
+                className="snap-start h-[100svh] w-screen pt-14 bg-cyan-300 flex flex-col"
             >
                 <h1 className="text-4xl mb-2 font-bold text-center">Skills</h1>
-                <div className="h-full w-full grid-rows-7 lg:grid-rows-10 grid grid-cols-1 gap-4 bg-cyan-300 p-4 lg:grid-cols-2"
+                <div className="h-full w-full grid-rows-7 lg:grid-rows-10 grid grid-cols-1 gap-4 px-4 pb-4 lg:grid-cols-2"
                 >
                     <div className="col-span-1 row-span-3 flex flex-col lg:col-span-1 lg:row-span-6">
                         <h2 className="mb-2 text-center text-xl font-bold">

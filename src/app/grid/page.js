a102e24ac080f7b35f079b585e4bcfbfd7abb13c
Artem Furman <artemfurman@gmail.com>
2023-08-14 05:16:21 +0200

    made different layput for small and big screen

--- src/app/grid/page.js
+++ src/app/grid/page.js
@@ -1,31 +1,36 @@
 export default function Home() {
     return (
-        <main>
-            <section id="skills-section" className="bg-cyan-300 w-screen h-screen grid grid-rows-10 gap-4 p-4">
-                <div className="row-span-6 grid grid-cols-2 gap-4">
-                    <div className="bg-blue-200 border-2 border-black rounded-2xl col-span-1 h-full w-full grid grid-rows-2 grid-cols-3 gap-2 p-2">
-                        <div className="bg-purple-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-yellow-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-pink-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-orange-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-gray-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-indigo-200 border-2 border-black rounded-2xl"></div>
-                    </div>
-                    <div className="bg-red-200 border-2 border-black rounded-2xl col-span-1 h-full w-full grid grid-rows-2 grid-cols-3 gap-2 p-2">
-                        <div className="bg-green-300 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-pink-300 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-teal-200 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-purple-300 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-yellow-300 border-2 border-black rounded-2xl"></div>
-                        <div className="bg-indigo-300 border-2 border-black rounded-2xl"></div>
-                    </div>
-                </div>
-                <div className="bg-green-200 border-2 border-black rounded-2xl row-span-4 h-full w-full grid grid-cols-3 gap-2 p-2">
-                    <div className="bg-red-300 border-2 border-black rounded-2xl"></div>
-                    <div className="bg-blue-300 border-2 border-black rounded-2xl"></div>
-                    <div className="bg-yellow-400 border-2 border-black rounded-2xl"></div>
-                </div>
-            </section>
-        </main>
+      <main>
+        <section id="skills-section" className="bg-cyan-300 w-screen h-screen grid grid-cols-1 lg:grid-cols-2 grid-rows-7 lg:grid-rows-10 gap-4 p-4">
+          <div className="row-span-3 lg:row-span-6 col-span-1 lg:col-span-1">
+            <div className="bg-blue-200 border-2 border-black rounded-2xl h-full w-full grid grid-rows-2 grid-cols-3 gap-2 p-2">
+              <div className="bg-purple-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-yellow-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-pink-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-orange-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-gray-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-indigo-200 border-2 border-black rounded-2xl"></div>
+            </div>
+          </div>
+          <div className="row-span-3 lg:row-span-6 col-span-1 lg:col-span-1">
+            <div className="bg-red-200 border-2 border-black rounded-2xl h-full w-full grid grid-rows-2 grid-cols-3 gap-2 p-2">
+              <div className="bg-green-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-pink-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-teal-200 border-2 border-black rounded-2xl"></div>
+              <div className="bg-purple-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-yellow-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-indigo-300 border-2 border-black rounded-2xl"></div>
+            </div>
+          </div>
+          <div className="row-span-4 col-span-1 lg:col-span-2">
+            <div className="bg-green-200 border-2 border-black rounded-2xl h-full w-full grid grid-cols-3 gap-2 p-2">
+              <div className="bg-red-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-blue-300 border-2 border-black rounded-2xl"></div>
+              <div className="bg-yellow-400 border-2 border-black rounded-2xl"></div>
+            </div>
+          </div>
+        </section>
+      </main>
     );
-}
+  }
+  

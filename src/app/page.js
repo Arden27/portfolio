bbd430638a32b882edc71531f84982d184013b83
Artem Furman <artemfurman@gmail.com>
2023-08-22 17:25:16 +0200

    portfolio section start

--- src/app/page.js
+++ src/app/page.js
@@ -23,15 +23,7 @@ export default function Small() {
                 id="home-section"
                 className="flex h-[100svh] w-screen snap-start items-center justify-center "
             >
-                <div className="flex h-5/6 w-full flex-col items-center justify-center md:h-screen md:flex-row">
-                    {/* <Image
-                        src="/img/profile_picture.png"
-                        alt="Profile Picture"
-                        sizes="100vh"
-                        className="h-[95%] w-auto object-cover hidden md:block"
-                        width={2832}
-                        height={4256}
-                    /> */}
+                <div className="flex md:gap-10 w-full h-5/6 flex-col items-center justify-center md:h-screen md:flex-row">
                     <div className="flex aspect-square h-2/3 items-center justify-center">
                         <Image
                             src="/img/about.jpeg"
@@ -42,7 +34,7 @@ export default function Small() {
                             height={720}
                         />
                     </div>
-                    <div className="md:ml-5 max-sm:flex max-sm:h-1/3 max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:p-4">
+                    <div className="max-sm:flex max-sm:h-1/3 md:w-1/3 max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:p-4">
                         <div className="text-lg font-semibold text-orange-600 md:mb-1">
                             Hello, I'm
                         </div>
@@ -52,13 +44,11 @@ export default function Small() {
                         <p className="text-md md:text-lg font-medium text-green-600 md:mb-1">
                             <span>Web Developer</span>
                         </p>
-                        <div className="mb-1 flex items-center justify-center md:mb-2 max-sm:w-4/5">
+                        <div className="mb-1 flex items-center justify-center md:mb-2 max-sm:w-4/5 md:w-4/4">
                             <p className="max-sm:text-center">
-                                I specialize in the creation and deployment of
-                                email marketing campaigns.
+                            I specialize in the creation and deployment of full-stack web applications, leveraging cutting-edge technologies
                             </p>
                         </div>
-
                         <a
                             href="#portfolio-section"
                             className="inline-block cursor-pointer rounded-lg bg-orange-600 px-5 py-2 font-medium text-white transition-all duration-200 ease-in-out hover:border hover:border-orange-600 hover:bg-white hover:text-orange-600"
@@ -87,9 +77,31 @@ export default function Small() {
             </section>
             <section
                 id="portfolio-section"
-                className="flex h-[100svh] w-screen snap-start items-center justify-center text-4xl"
+                className="flex h-[100svh] w-screen snap-start flex-col items-center justify-center pt-16 max-sm:pt-14"
             >
-                <h1>Portfolio</h1>
+                <h1 className="text-center text-4xl font-bold">
+                    Portfolio
+                </h1>
+                <div
+                    className="h-full w-full grid grid-cols-2 grid-rows-2 gap-4 pt-2 p-6"
+                >
+                    <div className="w-full h-full">
+                        <h2 className="mb-2 text-center text-xl font-bold">Project</h2>
+                        <div className="w-full h-5/6 rounded-2xl border-2 border-black bg-green-300"></div>    
+                    </div>
+                    <div className="w-full h-full">
+                        <h2 className="mb-2 text-center text-xl font-bold">Project</h2>
+                        <div className="w-full h-5/6 rounded-2xl border-2 border-black bg-green-300"></div>    
+                    </div>
+                    <div className="w-full h-full">
+                        <h2 className="mb-2 text-center text-xl font-bold">Project</h2>
+                        <div className="w-full h-5/6 rounded-2xl border-2 border-black bg-green-300"></div>    
+                    </div>
+                    <div className="w-full h-full">
+                        <h2 className="mb-2 text-center text-xl font-bold">Project</h2>
+                        <div className="w-full h-5/6 rounded-2xl border-2 border-black bg-green-300"></div>    
+                    </div>
+                </div>
             </section>
             <section
                 id="about-section"

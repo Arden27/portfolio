6eae2918f2e9e3d020ce6980bc6051f4238dcb10
Artem Furman <artemfurman@gmail.com>
2023-09-03 22:01:40 +0200

    regular hover fixes

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -9,15 +9,15 @@ export default function ProjectBox({ title, description, stack, placeSelfEnd }){
         onMouseEnter={() => setEaseIn(true)}
         onMouseLeave={() => setEaseIn(false)}
       >
-        <div className={`${placeSelfEnd ? 'md:group-hover:-translate-x-full' : 'md:group-hover:translate-x-full'} max-sm:hidden h-full md:absolute md:inset-0 rounded-2xl border-2 border-black bg-gray-100/50 md:transition-transform md:duration-700 md:ease-in-out md:flex md:items-center md:justify-center`}>
+        <div className={`${placeSelfEnd ? 'md:group-hover:-translate-x-full' : 'md:group-hover:translate-x-full'} max-sm:hidden h-full md:absolute md:inset-0 rounded-2xl border-2 border-black bg-gray-100/25 group-hover:bg-gray-100/50 md:transition md:duration-[800ms] md:ease-in-out md:flex md:items-center md:justify-center`}>
           <h2 className="mb-2 text-center text-xl font-medium peer-hover:opacity-100">{title}</h2>
         </div>
-        <div className={`text-center opacity-0 peer group-hover:opacity-100 transition-opacity  ${easeIn ? 'duration-[500ms] md:ease-linear' : 'duration-[800ms] md:ease-in'} text-clamp_project_description p-2 md:absolute md:inset-0 w-full h-full flex flex-col justify-between rounded-2xl border-2 border-black bg-gray-100/50`}>
-          <h3 className="mb-1 font-medium">{title}</h3>
-          <div className="h-4/5 overflow-scroll font-normal">
+        <div className={`text-center peer  ${easeIn ? 'duration-[500ms] md:ease-linear' : 'duration-[800ms] md:ease-in'} text-clamp_project_description p-2 md:absolute md:inset-0 w-full h-full flex flex-col justify-between rounded-2xl border-2 border-black bg-gray-100/25 group-hover:bg-gray-100/50`}>
+          <h3 className={`opacity-0 group-hover:opacity-100 transition-opacity ${easeIn ? 'duration-[500ms] md:ease-linear' : 'duration-[800ms] md:ease-in'} mb-1 font-medium`}>{title}</h3>
+          <div className={`opacity-0 group-hover:opacity-100 transition-opacity ${easeIn ? 'duration-[500ms] md:ease-linear' : 'duration-[800ms] md:ease-in'} h-4/5 overflow-scroll font-normal`}>
             {description}
           </div>
-          <div className="h-[13%] w-full flex flex-row  items-center justify-around">
+          <div className={`opacity-0 group-hover:opacity-100 transition-opacity ${easeIn ? 'duration-[500ms] md:ease-linear' : 'duration-[800ms] md:ease-in'} h-[13%] w-full flex flex-row  items-center justify-around`}>
             {stack.map((tech, index) => (
               <div className="relative h-full w-auto group/tech hover:cursor-pointer" key={index}>
                 {/* Text to show on hover */}

636cef8d9d8899a05e803e2a78f76e433f76ec10
Artem Furman <artemfurman@gmail.com>
2023-09-04 16:05:51 +0200

    chat window added

--- src/components/chat.js
+++ src/components/chat.js
@@ -3,18 +3,7 @@
 import { useState } from "react"
 import ChatIcon from "../../public/img/chat.svg"
 
-const tailwindColors = {
-    "text-red-50": "rgba(254, 226, 226, 1)",
-    "text-red-50/20": "rgba(254, 226, 226, 0.2)",
-    "text-gray-100": "rgba(243, 244, 246, 1)",
-    "text-gray-100/50": "rgba(243, 244, 246, 0.5)",
-    "text-gray-200": "rgba(229, 231, 235, 1)",
-    "text-gray-200/50": "rgba(229, 231, 235, 0.5)",
-    "text-white": "rgba(255, 255, 255, 1)",
-    "custom": "rgba(249, 115, 22, .8)"
-  };
-
-const chatIconColor = "rgba(249, 115, 22, .9)"
+const chatIconColor = "rgba(249, 115, 22, .8)"
 
 export default function Chat(){
 
@@ -26,14 +15,15 @@ export default function Chat(){
 
     return (
         <div className="z-50" >
-            <div className={`${open ? "" : "hidden"} z-50 border rounded-xl border-black aspect-square h-[50px] fixed bottom-10 right-6 border-x-teal-200/40`}>
-                chatbox
+            <div className={`${open ? "" : "hidden"} p-1 flex flex-col z-50 border rounded-xl border-black h-[85vh] w-[90vw] fixed bottom-16 right-6 bg-gray-100/50`}>
+                <div className="flex-grow bg-red-100/80 border rounded-xl border-black mb-1">messages box</div>
+                <div className="h-14 w-full bg-green-200/80 border rounded-xl border-black">chatinput box</div>
             </div>
             <button 
-                className="z-50 fixed bottom-2 right-5"
+                className="z-50 fixed bottom-1 right-5"
                 onClick={() => handleChatButton()}
             >
-                <ChatIcon className="border border-black rounded-xl bg-gray-100/50 text-orange-500 p-1 px-2 transform scale-x-[-1] w-16 h-auto opacity-75 text-red-50" fill={chatIconColor} />
+                <ChatIcon className="border border-black rounded-xl bg-gray-100/50 hover:bg-gray-100/80 p-1 px-2 transform scale-x-[-1] w-16 h-auto" fill={chatIconColor} />
             </button>
         </div>
     )

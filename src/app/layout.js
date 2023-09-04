410c72ff12b441585b6eb6dd0402c9dc680e357c
Artem Furman <artemfurman@gmail.com>
2023-09-04 20:27:14 +0200

    body fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,12 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className="overflow-hidden h-[100svh]" lang="en">
-      <body className={` h-[100svh]`}>
+      <body className={`fixed h-[100svh]`}>
         <div className="relative h-[100svh]">
           <div className="absolute inset-0 z-0">
             <DynamicBackground />
           </div>
-          <div className="relative z-10 h-[100svh]">
+          <div className="fixed z-10 h-[100svh]">
             {children}
           </div>
           <div className='z-20'>

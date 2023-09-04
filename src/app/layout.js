8afd41776ed303b68f6d9588c01abcf2b33338e4
Artem Furman <artemfurman@gmail.com>
2023-09-04 19:55:21 +0200

    html and body 100dvh

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className="overflow-hidden" lang="en">
-      <body className={inter.className}>
+    <html className="overflow-hidden h-[100dvh]" lang="en">
+      <body className={`${inter.className} h-[100dvh]`}>
         <div className="relative">
           <div className="absolute inset-0 z-0">
             <DynamicBackground />

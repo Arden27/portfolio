46a8f30a67baa5d1e9aaca756b4b4e4aa6372108
Artem Furman <artemfurman@gmail.com>
2023-09-04 16:08:46 +0200

    chat window svh added

--- src/components/chat.js
+++ src/components/chat.js
@@ -15,7 +15,7 @@ export default function Chat(){
 
     return (
         <div className="z-50" >
-            <div className={`${open ? "" : "hidden"} p-1 flex flex-col z-50 border rounded-xl border-black h-[85vh] w-[90vw] fixed bottom-16 right-6 bg-gray-100/50`}>
+            <div className={`${open ? "" : "hidden"} p-1 flex flex-col z-50 border rounded-xl border-black h-[85svh] w-[90vw] fixed bottom-16 right-6 bg-gray-100/50`}>
                 <div className="flex-grow bg-red-100/80 border rounded-xl border-black mb-1">messages box</div>
                 <div className="h-14 w-full bg-green-200/80 border rounded-xl border-black">chatinput box</div>
             </div>

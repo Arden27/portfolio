6ead5ab09d66388e385be5637d8355c8e616362b
Artem Furman <artemfurman@gmail.com>
2023-09-08 09:14:44 +0200

    very short screen fixed

--- src/components/skills/otherSkillsItem.js
+++ src/components/skills/otherSkillsItem.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const OtherSkillsItem = ({ src, alt, name }) => {
     return (
-        <div className="flex flex-col very-short:flex-row very-short:gap-1 h-full w-1/3  items-center justify-center">
-            <div className="relative  aspect-square  block items-center justify-center max-h-20 max-sm:h-1/2 h-2/3 rounded-2xl">
+        <div className="flex flex-col h-full w-1/3  items-center justify-center">
+            <div className="relative  aspect-square  block items-center justify-center max-h-20 max-sm:h-2/2 h-2/3 rounded-2xl">
                 <Image src={src} alt={alt} fill sizes="20vw" />
             </div>
-            <p className="very-short:text-xs">{name}</p>
+            <p className="">{name}</p>
         </div>
     );
 }

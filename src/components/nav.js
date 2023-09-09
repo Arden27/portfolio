042af1850f03a4a8ea86dfa6d600c864288a51db
Artem Furman <artemfurman@gmail.com>
2023-09-09 15:57:10 +0200

    nav shadow disabled on  desktop safari fixed

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ const NavBar = ({ mainRef }) => {
     const [shadow, setShadow] = useState(false);
     const [scrollInitiator, setScrollInitiator] = useState(null); // Add this to track which link initiated the scroll
 
-    const isSafari = typeof navigator !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && !navigator.userAgentData.mobile;
+    const isSafari = typeof navigator !== 'undefined' && /^((?!chrome|android).)*safari/i.test(navigator.userAgent) && !/Mobi|Android/i.test(navigator.userAgent);
 
     const handleScroll = () => {
         if (!mainRef.current) return;

0f9be0247daf815994d1938c248edcb0a2686f7c
Artem Furman <artemfurman@gmail.com>
2023-09-09 06:26:04 +0200

    links added

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -1,71 +1,91 @@
 import Image from "next/image";
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { openChat, closeChat } from "@/redux/store";
 
-export default function ProjectBox({ title, description, stack, placeSelfEnd }){
+export default function ProjectBox({ title, description, stack, placeSelfEnd, link, isLocal }){
   const [easeIn, setEaseIn] = useState(true);
+  const isChatOpen = useSelector((state) => state.isChatOpen);
+
+  const dispatch = useDispatch();
 
   const getOpacityClass = () => {
     return easeIn ? 'duration-[800ms] md:ease-in' : 'duration-[800ms] md:ease-in';
   };
 
-    return(
-    <div className={`group md:relative md:aspect-square max-sm:w-full max-sm:flex-shrink-0 max-sm:snap-center h-full ${placeSelfEnd ? 'place-self-end' : ''} md:hover:cursor-pointer`}>
-      <div className="h-full"
-        onMouseEnter={() => setEaseIn(true)}
-        onMouseLeave={() => setEaseIn(false)}
-      >
-        <div className={`${placeSelfEnd ? 'md:group-hover:-translate-x-full' : 'md:group-hover:translate-x-full'} max-sm:hidden h-full md:absolute md:inset-0 rounded-2xl border border-black bg-gray-100/25 group-hover:bg-gray-100/50 md:transition md:duration-[800ms] md:ease-in-out md:flex md:items-center md:justify-center`}>
-          <h2 className="mb-2 text-center text-xl font-medium peer-hover:opacity-100">{title}</h2>
-        </div>
-        <div className={`text-center peer  ${getOpacityClass()} max-sm:text-[5vw] md:text-clamp_project_description p-2 md:absolute md:inset-0 w-full h-full flex flex-col justify-between rounded-2xl border border-black md:bg-gray-100/25 max-sm:bg-gray-100/50 md:group-hover:bg-gray-100/50`}>
-          <h3 className={`md:opacity-0 md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} mb-1 font-medium`}>{title}</h3>
-          <div className={`md:opacity-0 max-sm:text-clamp_sm_project_description md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} : 'duration-[800ms] md:ease-in'} h-4/5 overflow-scroll font-normal`}>
-            {description}
-          </div>
-          <div className={`md:opacity-0 md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} h-[13%] w-full flex flex-row  items-center justify-around`}>
-            {stack.map((tech, index) => (
-              <div className="relative h-full w-auto group/tech hover:cursor-pointer" key={index}>
-                {/* Text to show on hover */}
-                <div className="absolute w-auto bottom-full text-center opacity-0 group-hover/tech:opacity-100 transition -translate-x-1/2 left-1/2">
-                  {tech}
-                </div>
-                {/* Image */}
-                <div className="relative flex items-center justify-center aspect-square h-3/4 rounded-2xl">
-                  <Image
-                    src={`/img/skills/${tech}.png`}
-                    alt={`${tech} icon`}
-                    className="h-full w-auto"
-                    width={200}
-                    height={200}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-
-          {/* <div className="h-[13%] w-full bg-amber-200 flex flex-row  items-center justify-around">
-            {stack.map((tech, index) => (
-              <div className="relative h-full w-auto group/tech hover:cursor-pointer" key={index}>
-                
-                <div className="absolute bg-white border rounded-lg  w-auto bottom-full text-center opacity-0 group-hover/tech:opacity-100 transition -translate-x-1/2 left-1/2">
-                  {tech}
-                </div>
-                
-                <div className="relative bg-indigo-300 block aspect-square h-2/3 rounded-2xl">
-                  <Image
-                    src={`/img/skills/${tech}.png`}
-                    alt={`${tech} icon`}
-                    className="h-full w-auto object-cover object-center"
-                    fill={true}
-                  />
-                </div>
-              </div>
-            ))}
-          </div> */}
-        </div>
-      </div>
-    </div>
-    );
+  const handleClick = () => {
+    if (isLocal) {
+        isChatOpen ? dispatch(closeChat()) : dispatch(openChat());
+    } else {
+        if (typeof window !== 'undefined') {
+            window.open(link, '_blank');
+        }
+    }
   };
+
+  return(
+    
+      <div className={`group md:relative md:aspect-square max-sm:w-full max-sm:flex-shrink-0 max-sm:snap-center h-full ${placeSelfEnd ? 'place-self-end' : ''} md:hover:cursor-pointer`}>
+        
+        <div className="h-full"
+          onMouseEnter={() => setEaseIn(true)}
+          onMouseLeave={() => setEaseIn(false)}
+          onClick={handleClick}
+        >
+          <div className={`${placeSelfEnd ? 'md:group-hover:-translate-x-full' : 'md:group-hover:translate-x-full'} max-sm:hidden h-full md:absolute md:inset-0 rounded-2xl border border-black bg-gray-100/25 group-hover:bg-gray-100/50 md:transition md:duration-[800ms] md:ease-in-out md:flex md:items-center md:justify-center`}>
+            <h2 className="mb-2 text-center text-xl font-medium peer-hover:opacity-100">{title}</h2>
+          </div>
+          <div className={`text-center peer  ${getOpacityClass()} max-sm:text-[5vw] md:text-clamp_project_description p-2 md:absolute md:inset-0 w-full h-full flex flex-col justify-between rounded-2xl border border-black md:bg-gray-100/25 max-sm:bg-gray-100/50 md:group-hover:bg-gray-100/50`}>
+            <h3 className={`md:opacity-0 md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} mb-1 font-medium`}>{title}</h3>
+            <div className={`md:opacity-0 max-sm:text-clamp_sm_project_description md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} : 'duration-[800ms] md:ease-in'} h-4/5 overflow-scroll font-normal`}>
+              {description}
+            </div>
+            <div className={`md:opacity-0 md:group-hover:opacity-100 transition-opacity ${getOpacityClass()} h-[13%] w-full flex flex-row  items-center justify-around`}>
+              {stack.map((tech, index) => (
+                <div className="relative h-full w-auto group/tech hover:cursor-pointer" key={index}>
+                  {/* Text to show on hover */}
+                  <div className="absolute w-auto bottom-full text-center opacity-0 group-hover/tech:opacity-100 transition -translate-x-1/2 left-1/2">
+                    {tech}
+                  </div>
+                  {/* Image */}
+                  <div className="relative flex items-center justify-center aspect-square h-3/4 rounded-2xl">
+                    <Image
+                      src={`/img/skills/${tech}.png`}
+                      alt={`${tech} icon`}
+                      className="h-full w-auto"
+                      width={200}
+                      height={200}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* <div className="h-[13%] w-full bg-amber-200 flex flex-row  items-center justify-around">
+              {stack.map((tech, index) => (
+                <div className="relative h-full w-auto group/tech hover:cursor-pointer" key={index}>
+                  
+                  <div className="absolute bg-white border rounded-lg  w-auto bottom-full text-center opacity-0 group-hover/tech:opacity-100 transition -translate-x-1/2 left-1/2">
+                    {tech}
+                  </div>
+                  
+                  <div className="relative bg-indigo-300 block aspect-square h-2/3 rounded-2xl">
+                    <Image
+                      src={`/img/skills/${tech}.png`}
+                      alt={`${tech} icon`}
+                      className="h-full w-auto object-cover object-center"
+                      fill={true}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div> */}
+          </div>
+        </div>
+        
+      </div>
+    
+  );
+};
 
   
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,25 +5,32 @@ const projects = [
         title: "Honey Marketplace",
         description: "A robust e-commerce platform dedicated to the sale of premium honey products. Designed the UI/UX and implemented features such as product categorization, search functionality, and secure payment gateway. Collaborated closely with a local honey producer to translate business requirements into technical solutions.",
         stack: ['tailwind', 'js', 'next', 'react', 'redux', 'python', 'django'],
-        placeSelfEnd: true
+        placeSelfEnd: true,
+        link: "https://artman.pythonanywhere.com/receiptapp",
+        isLocal: false,
     },
     {
         title: "ExpenseTracker+",
         description: "A financial management app aimed at simplifying the expense tracking process. Current features include the ability to add, edit, and categorize receipts. Future releases are planned to incorporate OCR technology for automatic receipt scanning and data extraction.",
         stack: ['tailwind', 'js', 'next', 'react', 'redux', 'python', 'django'],
-        placeSelfEnd: false
+        placeSelfEnd: false,
+        link: "https://artman.pythonanywhere.com/receiptapp",
+        isLocal: false,
     },
     {
         title: "CryptoCompounder",
         description: "A specialized financial calculator that showcases the accumulative power of daily investments in Bitcoin. Users input a fixed daily investment amount and the tool calculates the projected returns based on the current Bitcoin price. Designed to educate on the potential of consistent cryptocurrency investments.",
-        stack: ['tailwind', 'js', 'next', 'react', 'redux', 'python', 'django'],
-        placeSelfEnd: true
+        stack: ['html', 'css', 'js', 'jquery', 'python', 'django'],
+        placeSelfEnd: true,
+        link: "https://artman.pythonanywhere.com/flaws",
+        isLocal: false,
     },
     {
-        title: "ChatGPT Avatar",
+        title: "Avatar",
         description: "A virtual chatbot avatar that mimics my personality and can answer questions about me, serving as an interactive resume. Employed Natural Language Processing (NLP) techniques to create a more engaging and informative experience for users interested in my background and skills.",
-        stack: ['tailwind', 'js', 'next', 'react', 'redux', 'python', 'django'],
-        placeSelfEnd: false
+        stack: ['tailwind', 'js', 'next', 'react', 'redux', 'openai'],
+        placeSelfEnd: false,
+        isLocal: true,
     },
 ]
 
@@ -33,7 +40,7 @@ export default function Projects(){
             className="h-[95%] w-full max-w-[1440px] max-h-[800px] max-sm:flex max-sm:snap-x max-sm:snap-mandatory max-sm:flex-row max-sm:overflow-scroll md:grid md:grid-cols-2 md:grid-rows-2 md:gap-4 md:pt-2 p-4 md:p-6"
         >
             {projects.map((project, index) => (
-                <ProjectBox key={index} title={project.title} description={project.description} stack={project.stack} placeSelfEnd={project.placeSelfEnd} />
+                <ProjectBox key={index} {...project} />
             ))}
         </div>
     )

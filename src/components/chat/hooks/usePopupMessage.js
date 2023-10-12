71f16349ba271858857539bcd7c4a6039befd994
Artem Furman <artemfurman@gmail.com>
2023-10-12 08:18:24 +0200

    usePopupMessage hook added

--- src/components/chat/hooks/usePopupMessage.js
+++ src/components/chat/hooks/usePopupMessage.js
@@ -1,8 +1,11 @@
+// hook for triggering popup mssage
+
 import { useState, useEffect } from "react";
 
 export default function usePopupMessage(messages, isChatOpen) {
     const [showMessage, setShowMessage] = useState(false);
-  
+    
+    // when message array is changing and the chat window is closed return true for selected time
     useEffect(() => {
       if (!isChatOpen && messages.length > 0 && messages[messages.length - 1].role === 'assistant') {
         setShowMessage(true);

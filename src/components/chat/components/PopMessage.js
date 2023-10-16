7c37d2b4c1edce7272cb03508668398055d59ec0
Artem Furman <artemfurman@gmail.com>
2023-10-16 21:12:18 +0200

    slider added to popup message

--- src/components/chat/components/PopMessage.js
+++ src/components/chat/components/PopMessage.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 export default function PopMessage({ showMessage, isChatOpen, messages }) {
+  const wasPopped = useState(false)
   return (
     <div
-      className={`scrollable-chat fixed bottom-[9svh] right-5 z-50 max-h-[40%] max-w-[40%] overflow-scroll break-words rounded-bl-xl rounded-tl-xl rounded-tr-xl border border-violet-700/70 bg-gray-100/70 p-2 text-gray-800 md:max-w-[25%]
+      className={`scrollable-chat fixed bottom-[9svh] right-0 z-50 max-h-[40%] max-w-[40%] overflow-scroll break-words rounded-bl-xl rounded-tl-xl rounded-tr-xl border border-violet-700/70 bg-gray-100/70 p-2 text-gray-800 md:max-w-[25%]
         ${
           showMessage && !isChatOpen
-            ? "block"
-            : "hidden opacity-0 transition-opacity duration-[4000ms]"
+            ? "slide-in-right"
+            : wasPopped
+            ? "slide-out-right"
+            : "hidden"
         }`}
     >
       {messages.length > 0 && messages[messages.length - 1].content}
